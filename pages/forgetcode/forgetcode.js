e0fa4748aa78cbb8f954798f30af0fe1bbba39d1
iyang9816 <46879079+iyang9816@users.noreply.github.com>
2019-08-19 22:51:29 +0800

    Update forgetcode.js

--- pages/forgetcode/forgetcode.js
+++ pages/forgetcode/forgetcode.js
@@ -3,76 +3,101 @@ const app = getApp()
 var random = require("../../mock/random.js")
 Page({
   data: {
-    yangyi: random,
+    Random: random,
+    email:'',
+    password:'',
+    repassword:''
   },
   send_verify: function (event) {
     var that = this;
-    console.log(this.data.yangyi)
-    console.log(event.detail.value.phone);
+    // console.log(this.data.yangyi)
+    // console.log(event.detail.value.phone);
     that.setData({
-      phone: event.detail.value.phone,
+      email: event.detail.value.email,
     });
-
-    // wx.request({
-    //   url: 'https://www.gaoliuxu.com/index.php/Home/Index/send_verify',
-    //   data: {
-    //     phone: that.data.phone,
-    //   },
-    //   method: 'GET',
-    //   success: function (res) {
-    //     // success
-    //     if (res.data.errorcode == 0) {
-    //       wx.showToast({
-    //         title: '发送成功',
-    //         icon: 'success',
-    //         duration: 1500
-    //       })
-    //     } else {
-    //       wx.showToast({
-    //         title: res.data.message,
-    //         icon: 'success',
-    //         duration: 1500
-    //       })
-    //     }
-    //   }
-    // })
+    let regEmail = /^[a-z\d_\-\.]+@[a-z\d_\-]+\.[a-z\d_\-]+$/i;
+    if (!regEmail.test(this.data.email)) {
+      wx.showModal({
+        title: '错误',
+        content: '邮箱输入错误',
+      })
+    }
+    else{
+      wx.request({
+       url: 'https://www.ishclass.cn/recruit/login/proof',
+       data: {
+         mail: this.data.email,
+         text:this.data.Random
+       },
+       method: 'post',
+       header: {
+         'content-type': 'application/x-www-form-urlencoded'  //默认值  
+       },
+       success: function (res) {
+         console.log(res)
+         if(res.data==true){
+           wx.showToast({
+            title: '发送成功',
+            icon: 'success',
+            duration: 1500
+           })
+         }
+       }
+     })
+    }
   },
 
   register: function (event) {
     var that = this;
-    // wx.request({
-    //   url: 'https://www.gaoliuxu.com/index.php/Home/Index/forgetPassword',
-    //   data: {
-    //     phone: event.detail.value.phone,
-    //     password: event.detail.value.password,
-    //     verify_code: event.detail.value.verify_code,
-    //     repassword: event.detail.value.repassword,
-    //   },
-    //   method: 'POST',
-    //   header: {
-    //     "Content-Type": "application/x-www-form-urlencoded",
-    //   },
-    //   success: function (res) {
-    //     if (res.data.errorcode == 0) {
-    //       wx.showToast({
-    //         title: '修改成功',
-    //         icon: 'success',
-    //         duration: 1500
-    //       })
-    //       setTimeout(function () {
-    //         wx.navigateBack({
-    //           delta: 1
-    //         })
-    //       }, 1500);
-    //     } else {
-    //       wx.showToast({
-    //         title: res.data.message,
-    //         icon: 'success',
-    //         duration: 1500
-    //       })
-    //     }
-    //   }
-    // })
+    var email=event.detail.value.email
+    var password=event.detail.value.password
+    var repassword=event.detail.value.repassword
+    var code=event.detail.value.verify_code
+    var num=this.data.Random
+    if(password!=repassword){
+      wx.showToast({
+        title: '密码前后不一致',
+      })
+    }
+    else if(num!=code){
+wx.showToast({
+  title: '验证码错误',
+})
+    }
+    else{
+     wx.request({
+       url: 'https://www.ishclass.cn/recruit/login/passwordchange',
+       data: {
+         mail: event.detail.value.email,
+         newpassword: event.detail.value.password,
+       },
+      method: 'put',
+       header: {
+         'content-type': "application/x-www-form-urlencoded" // 默认值 
+       },
+       success: function (res) {
+         console.log(res)
+        if (res.data==true) {
+          wx.showToast({
+            title: '修改成功',
+            icon: 'success',
+            duration: 1500
+          })
+          setTimeout(function () {
+            wx.navigateBack({
+              delta: 1
+            })
+          }, 1500);
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'success',
+            duration: 1500
+          })
+        }
+       }
+     })
+    }
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
