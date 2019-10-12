b5d1965236fb53eae648583f937a98d70621123f
YatomigaAkashi <1660378230@qq.com>
2019-10-12 18:03:27 +0800

    详情页

--- pages/content/content.js
+++ pages/content/content.js
@@ -6,20 +6,21 @@ Page({
    */
   data: {
     "item": {
-      "job": "岗位名称",
+      "job": "测试类人员",
       "job_introduction": "这里是内容",
-      "salary_type": "月薪",
       "salary_month": "15月",
-      "salary_number": "8000-10000",
+      "salary_number": "8000-9000",
       "seek_education": "本科生",
       "seek_number": "50人",
-      "meeting_time": "9月10日9时-11时",
-      "meeting_address": "陕西省西安市长安区XXX路XXX楼903",
+      "meeting_time": "9:00-11:00",
+      "meeting_day": "10月9日",
+      "meeting_address": "b403",
       "job_company": "XXX公司",
       "job_office": "XXX单位",
-      "job_address": "国内 浙江 杭州",
-      "contact_email": "XXX",
-      "contact_phonenumber": "12345678912"
+      "job_address": "国内 浙江",
+      "contact_email": "1660378230@qq.com",
+      "contact_phonenumber": "12345678912",
+      "show": "www.baidu.com"
     }
   },
 })
